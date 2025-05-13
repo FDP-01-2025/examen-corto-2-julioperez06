c4d9f4955d7bb7e0c8051a6d9fab202113073a81
julio cesar perez salazar <00019125@uca.edu.sv>
2025-05-13 10:12:16 -0600

    Add files via upload

---
+++ corto00019125.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+int main (void) { 
+// le pedimos la edad al usuario
+    int edad 
+    { 
+        << cout "ingrese su edad";
+
+        cin >> edad;
+
+        //determinar variables
+
+if(edad=0 || <=10 )
+<< cout "tu talla recomendada es 8"endl;
+ if else (edad= 11 || <= 15)
+ << cout "tu talla recomendada es 12"endl;
+else if (edad= 16 || <= 60)
+<< cout "tu talla recomendada es 16"endl;
+else (edad>= 61)
+<< cout "tu talla recomendada es 18"endl;
+
+    return 0;
+    } 
+
+    // ahora determinamos el genero del usuario para darle un listado de ropa
+    int genero 
+     << cout "si se identifica como hombre presione 1 endl, como mujer 2"
+     cin >> genero
+// ahora mostramos el estilo de ropa y descuentos segun el genero
+switch (genero)
+{
+    case 1;
+    cout << "Para Hombre (descuento del 30%):
+•    Camisa Casual: $90 (descuento del 30%).
+•    Pantalón Formal: $130 (descuento del 30%).
+•    Chaqueta Premium: $900 (descuento del 30%)"
+
+      case 2
+   cout << "Para Mujer (descuento del 20%):
+•    Blusa Moderna: $80 (descuento del 20%).
+•    Falda Elegante: $110 (descuento del 20%).
+•    Abrigo Premium: $250 (descuento del 20%)"
+
+ break;
+}
+
+<<cout "seleccione una lo que desea comprar
+
+}
